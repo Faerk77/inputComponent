dda99c3085fd43daed9c0c2ccc798b1f7a935977
Faerk77 <87774223+Faerk77@users.noreply.github.com>
2022-09-09 16:56:41 -0300

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,47 @@
+import { useState } from 'react';
 import './App.css';
 import Icon from './assets/Icon';
 import Input from './components/Input';
 
 function App() {
+  const [input, setInput] = useState('Text');
+
   return (
     <div className="App">
       <header>
         <h1 className="title">Inputs</h1>
       </header>
-      <Input type="Text" text="Label" placeholder="Placeholder" />
+      <Input
+        type="Text"
+        text="Label"
+        placeholder="Placeholder"
+        value={input}
+        onChange={(ev) => {
+          setInput(ev.target.value);
+        }}
+      />
       <Input type="Text" text="Label" placeholder="Placeholder" error />
       <Input type="Text" text="Label" placeholder="Placeholder" disabled />
-      <Input type="Text" text="Label" helperText='Some interesting text' placeholder="Placeholder" error />
-      <Input type="Text" text="Label" placeholder="Placeholder" startIcon={Icon} />
-      <Input paddingLeft='3rem' type="Text" text="Label" placeholder="Placeholder" endIcon={Icon} />
-
+      <Input
+        type="Text"
+        text="Label"
+        helperText="Some interesting text"
+        placeholder="Placeholder"
+        error
+      />
+      <Input
+        type="Text"
+        text="Label"
+        placeholder="Placeholder"
+        startIcon={Icon}
+      />
+      <Input
+        type="Text"
+        text="Label"
+        placeholder="Placeholder"
+        endIcon={Icon}
+      />
+      <Input multiline cols="8" row="8" />
     </div>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -9,15 +9,25 @@ const Input = ({
   startIcon: StartIcon,
   endIcon: EndIcon,
   fullWidth,
+  multiline,
   ...props
 }) => {
   const labelClass = style['inputLabel'];
   const inputClass = style['input'];
+  const inputContainer = style['inputContainer'];
+
+if(multiline){
+  return(
+    <div>
+        <textarea name="Text">{text}</textarea>
+    </div>
+  )
+}
 
   return (
     <label className={`${labelClass} ${error && style['inputLabelError']}`}>
       {text && <span>{text}</span>}
-      <div className={`${style['inputContainer']}`}>
+      <div className={`${inputContainer}`}>
         {StartIcon && (
         <span className={style.startIcon}> 
           <StartIcon  width={iconWidth} />
